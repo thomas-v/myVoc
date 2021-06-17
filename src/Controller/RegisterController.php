c102e8dc0d49e07aac13632201e889beb34b92b4
tvitiello <thomas.vitiello@outlook.fr>
2021-06-17 22:43:35 +0200

    form register view

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegisterType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RegisterController extends AbstractController
 {
@@ -13,6 +15,10 @@ class RegisterController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('register/index.html.twig');
+        $form = $this->createForm(RegisterType::class, new User());
+
+        return $this->render('register/index.html.twig',[
+            'form' => $form->createView()
+        ]);
     }
 }
