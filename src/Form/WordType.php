1a809dce73a972e6dd80a57103c3c7604de7caed
thomas-v <thomas.vitiello@outlook.fr>
2021-08-06 19:04:04 +0200

    form word

---
+++ src/Form/WordType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Word;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('value')
+            ->add('translation')
+            ->add('language_id')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Word::class,
+        ]);
+    }
+}
