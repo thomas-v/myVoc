cb79cc47fc1d7857803b8698eaa2d00c12d0b355
thomas-v <thomas.vitiello@outlook.fr>
2021-08-06 19:01:32 +0200

    word migration

---
+++ migrations/Version20210806170035.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210806170035 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE word (id INT AUTO_INCREMENT NOT NULL, language_id_id INT NOT NULL, user_id_id INT NOT NULL, value VARCHAR(255) NOT NULL, translation VARCHAR(255) NOT NULL, INDEX IDX_C3F175111C9A06 (language_id_id), INDEX IDX_C3F175119D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE word ADD CONSTRAINT FK_C3F175111C9A06 FOREIGN KEY (language_id_id) REFERENCES language (id)');
+        $this->addSql('ALTER TABLE word ADD CONSTRAINT FK_C3F175119D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE word');
+    }
+}
