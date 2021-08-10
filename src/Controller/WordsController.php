747f92867c030bdf40eeca520aa0e6dedbde42f7
thomas-v <thomas.vitiello@outlook.fr>
2021-08-10 22:57:50 +0200

    add word form

--- src/Controller/WordsController.php
+++ src/Controller/WordsController.php
@@ -2,12 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Word;
+use App\Form\WordType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class WordsController extends AbstractController
 {
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/mes-mots', name: 'words')]
     public function index(): Response
     {
@@ -15,10 +24,25 @@ class WordsController extends AbstractController
         ]);
     }
 
-    #[Route('/ajouter-un-mot', name: 'words-add')]
-    public function add(): Response
+    #[Route('/ajouter-un-mot', name: 'word-add')]
+    public function add(Request $request): Response
     {
+        $word = new Word();
+        $user = $this->getUser();
+        $form = $this->createForm(WordType::class, $word);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $word = $form->getData();
+            $word->setUserId($user);
+
+            $this->entityManager->persist($word);
+            $this->entityManager->flush(); 
+        }
+
         return $this->render('words/add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/WordType.php
+++ src/Form/WordType.php
@@ -3,18 +3,35 @@
 namespace App\Form;
 
 use App\Entity\Word;
+use App\Entity\Language;
+use App\Repository\LanguageRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class WordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $formOptions = [
+            'class' => Language::class,
+            'choice_label' => 'name',
+        ];
+
         $builder
             ->add('value')
             ->add('translation')
-            ->add('language_id')
+            ->add('language_id', EntityType::class, [
+                'class' => Language::class,
+                'choice_label' => 'name',
+                'label' => 'Choisissez une langue'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer'
+            ])
         ;
     }
 
